40589b073ce18415f600c10841703e005dce368b
ChumaDenis <chumiks1999@gmail.com>
2021-04-02 11:19:39 +0300

    First commit

---
+++ cmake-build-debug/CMakeFiles/lab10_2_.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab10_2_.dir/main.c.obj"
+  "lab10_2_.exe"
+  "lab10_2_.exe.manifest"
+  "lab10_2_.lib"
+  "lab10_2_.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab10_2_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
